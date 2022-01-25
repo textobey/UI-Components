552d04ece26df80a863de16c41d6e36e408d820a
textobey <dogsan_e@naver.com>
2022-01-25 17:30:34 +0900

    Develop network module

--- MyFoundationUIKit/MyFoundation/Features/NetworkTest/NetworkTestViewController.swift
+++ MyFoundationUIKit/MyFoundation/Features/NetworkTest/NetworkTestViewController.swift
@@ -7,8 +7,16 @@
 
 import UIKit
 import Moya
+import RxSwift
+import RxCocoa
+
+struct TestModel: Decodable {
+    let lat: String
+    let lon: String
+}
 
 class NetworkTestViewController: UIBaseViewController {
+    private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,7 +25,28 @@ class NetworkTestViewController: UIBaseViewController {
     }
     
     func requestCurrentWeahterAPI() {
-        let provider = MoyaProvider<API>()
+            
+        /*
+        API.currentWeather(lat: "37.48119118657402", lon: "126.88432643360242")
+            .request()
+            .map {
+                guard let value = try $0.mapString().data(using: .utf8) else { return $0 }
+                let newResponse = Response(
+                    statusCode: $0.statusCode,
+                    data: value,
+                    request: $0.request,
+                    response: $0.response
+                )
+                return newResponse
+            }
+            .map(CurrentWeather.self, using: API.jsonDecoder)
+            .asObservable()
+            .bind(onNext: { element in
+                print(element)
+            }).disposed(by: disposeBag)
+        */
+
+        /*let provider = MoyaProvider<API>()
         provider.request(.currentWeather(lat: "37.48119118657402", lon: "126.88432643360242")) { result in
             switch result {
             case .success(let response):
@@ -26,6 +55,6 @@ class NetworkTestViewController: UIBaseViewController {
             case .failure(let error):
                 print(error.localizedDescription)
             }
-        }
+        }*/
     }
 }
--- MyFoundationUIKit/MyFoundation/Networking/API+Request.swift
+++ MyFoundationUIKit/MyFoundation/Networking/API+Request.swift
@@ -23,10 +23,39 @@ extension API {
     }
     
     private enum MoyaWrapper {
+        struct Plugins {
+            var plugins: [PluginType]
+            
+            init(plugins: [PluginType] = []) {
+                self.plugins = plugins
+            }
+            
+            func callAsFunction() -> [PluginType] { self.plugins }
+        }
+        
         static var provider: MoyaProvider<API.Wrapper> {
-            return MoyaProvider<API.Wrapper>(endpointClosure: { target in
-                MoyaProvider.defaultEndpointMapping(for: target)
-            })
+            // EndPoint 인스턴스에 맵핑하는 클로저, 해당 클로저로 인해서, Generic 타입을 지정하여 스위프트에 알릴 필요가 없음.
+            let endpointClosure = { (target: API.Wrapper) -> Endpoint in
+                // URL(target:) 모든 TargetType에서 URL을 만들수있는
+                let url = URL(target: target).absoluteString
+                return Endpoint(url: url, sampleResponseClosure: {
+                    .networkResponse(200, target.sampleData)
+                },
+                method: target.method,
+                task: target.task,
+                httpHeaderFields: target.headers
+                )
+            }
+            
+            let plugins = Plugins(plugins: [])
+            
+            let configuration = URLSessionConfiguration.default
+            configuration.timeoutIntervalForRequest = 3
+            configuration.urlCredentialStorage = nil
+            let session = Session(configuration: configuration)
+            
+            // Moya Provider 인스턴스 생성
+            return MoyaProvider(endpointClosure: endpointClosure, session: session, plugins: plugins())
         }
     }
 }
@@ -39,29 +68,82 @@ extension API {
         return decoder
     }
     
-    func request<T: Decodable>(file: StaticString = #file, function: StaticString = #function, line: UInt = #line) -> Single<T> {
+    static func request<T: Decodable>(_ api: API) -> Single<T> {
+        //return api.request().map(T.self, using: API.jsonDecoder).observe(on: MainScheduler.instance)
         
-        return Single.create { observer in
-            let endPoint = API.Wrapper(base: self)
-            let requestString = "\(endPoint.method) \(endPoint.baseURL) \(endPoint.path)"
-            
-            return Self.moya.rx.request(endPoint)
-                .filterSuccessfulStatusCodes()
-                .do(onSuccess: { response in
-                    let requestContent = "🛰 SUCCESS: \(requestString) (\(response.statusCode))"
-                    print(requestContent, file, function, line)
-                }, onError: { error in
-                    print(error)
-                }).map {
-                    let jsonString = try $0.mapString()
-                    guard let value = jsonString.data(using: .utf8) else { return $0 }
-                    let newResponse = Response(statusCode: $0.statusCode, data: value, request: $0.request, response: $0.response)
-                    return newResponse
-                }
-                .map(T.self, using: API.jsonDecoder)
-                .map { result in
-                    
-                }
-        }
+        let single = Single<T>.create { observer in
+            let observable = api.request().map(T.self, using: API.jsonDecoder)
+                .subscribe(onSuccess: { data in
+                    observer(.success(data))
+                },onFailure: { error in
+                    print(error.localizedDescription)
+                })
+            return Disposables.create {
+                observable.dispose()
+            }
+        }.observe(on: MainScheduler.instance)
+        
+        return single
+    }
+    
+    private func request(
+        file: StaticString = #file,
+        function: StaticString = #function,
+        line: UInt = #line
+    ) -> Single<Response> {
+        let endPoint = API.Wrapper(base: self)
+        let requestString = "\(endPoint.method) \(endPoint.baseURL) \(endPoint.path)"
+        
+        return Self.moya.rx.request(endPoint)
+            .filterSuccessfulStatusCodes()
+            .do(onSuccess: { response in
+                let requestContent = "🛰 SUCCESS: \(requestString) (\(response.statusCode))"
+                print(requestContent, file, function, line)
+            }, onError: { error in
+                print(error)
+            }, onSubscribe: {
+                let message = "REQUEST: \(requestString)"
+                print(message, file, function, line)
+            }).map {
+                guard let value = try $0.mapString().data(using: .utf8) else { return $0 }
+                let newResponse = Response(
+                    statusCode: $0.statusCode,
+                    data: value,
+                    request: $0.request,
+                    response: $0.response
+                )
+                return newResponse
+            }
     }
 }
+
+//protocol DecodableTargetType: Moya.TargetType {
+//    associatedtype Response: Decodable
+//}
+
+/*static func request<T: Decodable, E>(
+    type: T.Type,
+    result: Result<Response, MoyaError>,
+    completion: @escaping (Result<E, Error>) -> Void
+) {
+    switch result {
+    case .success(let response):
+        let data = try? response.map(T.self, using: API.jsonDecoder)
+        completion(.success(data as! E))
+    case .failure(let error):
+        completion(.failure(error))
+    }
+}*/
+
+/*static func request<T: Decodable>(_ api: API) -> Single<T> {
+    return Single<T>.create { observer in
+        let observable = api.request().map(T.self, using: API.jsonDecoder)
+            .asObservable()
+            .subscribe(onNext: { data in
+                observer(.success(data))
+            })
+        return Disposables.create {
+            observable.dispose()
+        }
+    }.observe(on: MainScheduler.instance)
+}*/
