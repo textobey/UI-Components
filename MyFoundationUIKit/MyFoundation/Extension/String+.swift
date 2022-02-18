f3eb7cac552a85f0d8a120c6eb6b27ea94122bcb
textobey <dogsan_e@naver.com>
2022-02-18 18:59:26 +0900

    Added Good Extension

--- MyFoundationUIKit/MyFoundation/Extension/String+.swift
+++ MyFoundationUIKit/MyFoundation/Extension/String+.swift
@@ -8,6 +8,30 @@
 import Foundation
 
 extension String {
+    enum LetterType {
+        case english
+        case korean
+        case number
+        case unknown
+    }
+    
+    func getLetterType() -> LetterType {
+        if self < "A" {
+            return .number
+        }
+        // 둘 다 영문이면 오름차순 정렬
+        else if (self >= "A" && self <= "z") {
+            return .english
+        }
+        // 둘 다 한글이면 오름차순 정렬
+        else if self > "z" {
+            return .korean
+        }
+        return .unknown
+    }
+}
+
+extension String {
     /// "yyyy-MM-dd HH:mm:ss"
     func toDate() -> Date? {
         let dateFormatter = DateFormatter()
