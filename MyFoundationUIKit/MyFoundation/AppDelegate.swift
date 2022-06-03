02b8eecce693a0897eeb82a0226f08c90b5fbb5e
exponentChoi <charlie@channel.io>
2022-06-03 18:07:11 +0900

    [Fix] Guard문 줄바꿈 and Then 사용

--- MyFoundationUIKit/MyFoundation/AppDelegate.swift
+++ MyFoundationUIKit/MyFoundation/AppDelegate.swift
@@ -14,14 +14,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         guard let window = window else { return false }
-        let navigationController = UINavigationController(rootViewController: MainViewController())
-        navigationController.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
-        navigationController.navigationBar.shadowImage = UIImage()
-        navigationController.navigationBar.isTranslucent = true
-        navigationController.view.backgroundColor = UIColor.clear
-        navigationController.navigationBar.backgroundColor = UIColor.clear
-        navigationController.setNavigationBarHidden(true, animated: false)
-        
+      
+        let navigationController = UINavigationController(rootViewController: MainViewController()).then {
+            $0.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+            $0.navigationBar.shadowImage = UIImage()
+            $0.navigationBar.isTranslucent = true
+            $0.view.backgroundColor = UIColor.clear
+            $0.navigationBar.backgroundColor = UIColor.clear
+            $0.setNavigationBarHidden(true, animated: false)
+        }
+      
         window.overrideUserInterfaceStyle = .light
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
