bd4a7f31aa00dcc51e35920d66c5ec8a44af5a26
textobey <dogsan_e@naver.com>
2022-08-18 16:04:12 +0900

    Added Study Project

---
+++ SOLID/SOLID/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOLID
+//
+//  Created by 이서준 on 2022/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
