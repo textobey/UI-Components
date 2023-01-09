d79fec067fcf166e0069b4a25719b3473b856e39
textobey <dogsan_e@naver.com>
2023-01-09 14:51:59 +0900

    DynamicMemberLookup 맛보기

---
+++ DynamicMemberLookup_Practice/DynamicMemberLookup_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicMemberLookup_Practice
+//
+//  Created by 이서준 on 2023/01/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
