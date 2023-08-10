a1c4a87e042a5a7143d02d5c708beb9df8079ac3
textobey <dogsan_e@naver.com>
2023-08-10 15:50:48 +0900

    [ADD] Framework Practice

---
+++ TextoUI/TextoUI/TextoUI.h
@@ -0,0 +1,18 @@
+//
+//  TextoUI.h
+//  TextoUI
+//
+//  Created by 이서준 on 2023/08/10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TextoUI.
+FOUNDATION_EXPORT double TextoUIVersionNumber;
+
+//! Project version string for TextoUI.
+FOUNDATION_EXPORT const unsigned char TextoUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TextoUI/PublicHeader.h>
+
+
