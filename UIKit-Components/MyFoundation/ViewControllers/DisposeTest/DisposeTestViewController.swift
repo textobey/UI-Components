7186ddf9e06ea237aa3a3586f2744fe6d2bd8f9a
textobey <dogsan_e@naver.com>
2023-11-01 17:53:42 +0900

    [ADD] Memory leak TestCase

--- UIKit-Components/MyFoundation/ViewControllers/DisposeTest/DisposeTestViewController.swift
+++ UIKit-Components/MyFoundation/ViewControllers/DisposeTest/DisposeTestViewController.swift
@@ -47,28 +47,91 @@ class DisposeTestViewController: UIBaseViewController {
     }
     
     @objc func memoryLeakDisposable() {
-        Observable<String>.create({ observer in
-            observer.onNext("1")
-            // observer.onError(MyError.anError)
-            // observer.onCompleted()
-            return Disposables.create()
-        }).subscribe(
-            onNext: { [weak self] num in
-                self?.storage = num
-                self?.printString()
-            },
-            onError: { print($0) },
-            onCompleted: { print("Completed") },
-            onDisposed: { print("Disposed") }
-        )
-        //DisposeTestService.shared.createNeverEndingObservable()
-        //    .delay(.seconds(1), scheduler: MainScheduler.asyncInstance)
-        //    .subscribe(onNext: { element in
-        //        print(element)
-        //    })
+
     }
     
     func printString() {
         print("yoyoyoyo")
     }
 }
+
+//case1: 일반적인 Cold Observable 생성 후 subscribe -> Disposable 생성, dispose 처리 안함
+//result: Instruments를 활용한 메모리누수 확인
+//Observable<String>.create({ observer in
+//    observer.onNext("method: memoryLeakDisposable")
+//    return Disposables.create()
+//}).subscribe(
+//    onNext: { event in
+//        print(event)
+//    },
+//    onError: { print($0) },
+//    onCompleted: { print("Completed") },
+//    onDisposed: { print("Disposed") }
+//)
+
+//case2: Disposable 와일드카드패턴을 활용한 unuse 처리
+//result: Instruments를 활용한 메모리누수 확인
+//_ = Observable<String>.create({ observer in
+//    observer.onNext("method: memoryLeakDisposable")
+//    return Disposables.create()
+//}).subscribe(
+//    onNext: { event in
+//        print(event)
+//    },
+//    onError: { print($0) },
+//    onCompleted: { print("Completed") },
+//    onDisposed: { print("Disposed") }
+//)
+
+//case3: take(1) 오퍼레이터를 활용하여 1회의 이벤트 발생만을 보장하는 방식
+//result: 메모리누수 발생하지 않음
+//Observable<String>.create({ observer in
+//    observer.onNext("method: memoryLeakDisposable 1")
+//    observer.onNext("method: memoryLeakDisposable 2")
+//    return Disposables.create()
+//}).take(1).subscribe(
+//    onNext: { event in
+//        print(event)
+//    },
+//    onError: { print($0) },
+//    onCompleted: { print("Completed") },
+//    onDisposed: { print("Disposed") }
+//)
+
+//case4: Disposable 타입 프로퍼티를 선언하여 Disposable을 저장/관리하는 방식
+//result: 메모리누수 발생하지 않음
+//var disposable: Disposable?
+//
+//disposable?.dispose()
+//disposable = Observable<String>.create({ observer in
+//    observer.onNext("method: memoryLeakDisposable")
+//    // observer.onError(MyError.anError)
+//    // observer.onCompleted()
+//    return Disposables.create()
+//}).subscribe(
+//    onNext: { event in
+//        print(event)
+//    },
+//    onError: { print($0) },
+//    onCompleted: { print("Completed") },
+//    onDisposed: { print("Disposed") }
+//)
+
+//case5: CompositeDisposable 타입 프로퍼티를 선언하여 Disposable을 저장/관리하는 방식
+//result: 메모리누수 발생하지 않음
+//let compositeDisposable = CompositeDisposable()
+//
+//let disposable = Observable<String>.create({ observer in
+//    observer.onNext("method: memoryLeakDisposable")
+//    return Disposables.create()
+//}).subscribe(
+//    onNext: { event in
+//        print(event)
+//    },
+//    onError: { print($0) },
+//    onCompleted: { print("Completed") },
+//    onDisposed: { print("Disposed") }
+//)
+//disposeKey를 저장해두면 compositeDisposable에서 관리중인 특정 disposable만 콕 찝어 관리하는것이 가능한 장점
+//let disposeKey = compositeDisposable.insert(disposable)
+
